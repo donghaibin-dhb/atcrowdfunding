fe08c7a5cfd7f485f76454a63887b1161d4af2a1
donghb <dohaibin@163.com>
2020-07-01 07:43:12 +0800

    针对权限功能的完善2222111

--- atcrowdfunding-bean/src/main/java/com/dhb/atcrowdfunding/bean/Test.java
+++ atcrowdfunding-bean/src/main/java/com/dhb/atcrowdfunding/bean/Test.java
@@ -1,5 +1,7 @@
 package com.dhb.atcrowdfunding.bean;
 
+import java.sql.SQLOutput;
+
 /**
  * @ClassName: Test
  * @Description:
@@ -8,4 +10,8 @@ package com.dhb.atcrowdfunding.bean;
  * @Version 1.0
  */
 public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("....");
+    }
 }
